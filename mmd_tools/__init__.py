f78bba6e74cf6efe4b9b86f8ed7387f7566a2a80
yohawing <yohawing@example.com>
2026-07-02 12:02:00 +0900

    refactor: replace settings module alias

--- mmd_tools/__init__.py
+++ mmd_tools/__init__.py
@@ -2,8 +2,6 @@
 MMD Tools - Maya MMD import/export toolkit
 """
 
-import sys
-
 # パッケージのメタデータ
 __name__ = "mmd_tools"
 __version__ = "0.3.0"
@@ -16,10 +14,6 @@ from .core.pmd_data import PmdData
 from .core.pmx_data import PmxData
 from .core.vmd_data import VmdData
 from .core.settings import get_settings, settings
-from .core import settings as _settings_module
-
-# mmd_tools.settings としてアクセス可能にする
-sys.modules["mmd_tools.settings"] = _settings_module
 
 # 公開API
 __all__ = [
